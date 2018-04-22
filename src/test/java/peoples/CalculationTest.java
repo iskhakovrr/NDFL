cdc0b0a3b60e75737728d2babb228662c9628702
iskhakovrr <34068634+iskhakovrr@users.noreply.github.com>
2018-04-22 20:06:06 +0500

    asdg

--- src/test/java/peoples/CalculationTest.java
+++ src/test/java/peoples/CalculationTest.java
@@ -1,11 +1,24 @@
 package peoples;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculationTest {
+ Calculation testingClass;
 
+    @BeforeEach
+   void setUp() {
+        testingClass = new Calculation();
+    }
+
+   @AfterEach
+   void tearDown() {
+        testingClass = null;
+   }
+   
     @Test
     void calc_n() {
         assertEquals(1300, Calculation.Calc_n(13, 10000));
