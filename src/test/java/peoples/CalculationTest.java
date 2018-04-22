d86655138aab4ce053671ca684572673eee04a6f
iskhakovrr <34068634+iskhakovrr@users.noreply.github.com>
2018-04-22 20:14:28 +0500

    HAKAHEL|TA

--- src/test/java/peoples/CalculationTest.java
+++ src/test/java/peoples/CalculationTest.java
@@ -1,6 +1,5 @@
 package peoples;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,8 +17,39 @@ class CalculationTest {
     void calc_n() {
         assertEquals(1300, Calculation.Calc_n(13, 10000));
     }
+	
 	    @Test
     void calc_n2() {
         assertEquals(1500, Calculation.Calc_n(15, 10000));
+    }
+	
+    @Test
+    void calc_n() {
+        assertEquals(11500, Calculation.Calc_n(23, 50000));
+    }
+	
+	    @Test
+    void calc_n() {
+        assertEquals(4320, Calculation.Calc_n(18, 24000));
+    }
+	
+	@Test
+    void calc_n() {
+        assertEquals(630, Calculation.Calc_n(9, 7000));
+    }
+	    
+		@Test
+    void calc_n() {
+        assertEquals(1920, Calculation.Calc_n(12, 16000));
+    }
+	
+	@Test
+    void calc_n() {
+        assertEquals(3825, Calculation.Calc_n(17, 22500));
+    }
+	
+    @Test
+    void calc_n() {
+        assertEquals(39600, Calculation.Calc_n(33, 120000));
     }
 }
