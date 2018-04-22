582b05db3669d72e079efb4e3b3efb8f77c0b483
iskhakovrr <34068634+iskhakovrr@users.noreply.github.com>
2018-04-22 18:01:29 +0500

    23.45

--- src/test/java/peoples/CalculationTest1.java
+++ src/test/java/peoples/CalculationTest1.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculationTest1{
+class CalculationTest1 {
 
     @Test
     void calc_n() {
-        assertEquals(1500, Calculation.Calc_n(15, 10000));
+        assertEquals(1300, Calculation.Calc_n(13, 10000));
     }
 }
