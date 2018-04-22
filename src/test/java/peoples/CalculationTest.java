1151478bf192ebbb613241842c59e5dfbbd2fd25
iskhakovrr <34068634+iskhakovrr@users.noreply.github.com>
2018-04-22 20:15:58 +0500

    HAKAHEL|TA

--- src/test/java/peoples/CalculationTest.java
+++ src/test/java/peoples/CalculationTest.java
@@ -24,32 +24,32 @@ class CalculationTest {
     }
 	
     @Test
-    void calc_n() {
+    void calc_n3() {
         assertEquals(11500, Calculation.Calc_n(23, 50000));
     }
 	
 	    @Test
-    void calc_n() {
+    void calc_n4() {
         assertEquals(4320, Calculation.Calc_n(18, 24000));
     }
 	
 	@Test
-    void calc_n() {
+    void calc_n5() {
         assertEquals(630, Calculation.Calc_n(9, 7000));
     }
 	    
 		@Test
-    void calc_n() {
+    void calc_n6() {
         assertEquals(1920, Calculation.Calc_n(12, 16000));
     }
 	
 	@Test
-    void calc_n() {
+    void calc_n7() {
         assertEquals(3825, Calculation.Calc_n(17, 22500));
     }
 	
     @Test
-    void calc_n() {
+    void calc_n8() {
         assertEquals(39600, Calculation.Calc_n(33, 120000));
     }
 }
